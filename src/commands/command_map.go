32b232ac69c509cc696220bc74daa79e8d6caba3
Antoine Meresse <ant.meresse@gmail.com>
2023-12-08 18:48:49 +0100

    feat: command stamina

--- src/commands/command_map.go
+++ src/commands/command_map.go
@@ -3,10 +3,14 @@ package commands
 import commandslist "github.com/AntoineMeresse/flibot-urt/src/commands/commands_list"
 
 var Commands map[string]Command = map[string]Command {
+	// Level: 0
 	"play" : {commandslist.Play, 0, ""},
 	"spec" : {commandslist.Spec, 0, ""},
 	"currentmap" : {commandslist.CurrentMap, 0, ""},
 	"nextmap" : {commandslist.NextMap, 0, ""},
+	"stamina" : {commandslist.Stamina, 0, ""},
+
+	// Level: 80
 	"setgoto" : {commandslist.SetGoto, 80, ""},
 }
 
---
+++ src/commands/commands_list/stamina.go
@@ -0,0 +1,11 @@
+package commandslist
+
+import (
+	"fmt"
+
+	"github.com/AntoineMeresse/flibot-urt/src/models"
+)
+
+func Stamina(server models.Server, playerNumber string, params []string, isGlobal bool) {
+	server.Rcon.RconCommand(fmt.Sprintf("customstamina %s", playerNumber))
+}
