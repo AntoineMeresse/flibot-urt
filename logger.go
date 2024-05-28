b1c638376660e50ec061a1ae520abe9b9e948d7a
Antoine Meresse <ant.meresse@gmail.com>
2024-05-28 22:26:03 +0200

    fix: logging conf in seperate file

---
+++ logger.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path"
+	"runtime"
+	"strconv"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func configureLogger() {
+	log.SetLevel(log.DebugLevel)
+	log.SetReportCaller(true)
+	log.SetFormatter(&log.TextFormatter{
+		FullTimestamp: true,
+		CallerPrettyfier: func(frame *runtime.Frame) (function string, file string) {                                                     
+			fileName := " " + path.Base(frame.File) + ":" + strconv.Itoa(frame.Line)  + " | "      
+			//return frame.Function, fileName                                        
+			return "", fmt.Sprintf("%25.25s", fileName)                                                      
+		},    
+		DisableLevelTruncation: true,
+		PadLevelText: true,
+	})
+	log.SetOutput(os.Stdout)
+}
--- main.go
+++ main.go
@@ -1,42 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"path"
-	"runtime"
-	"strconv"
 	"time"
 
 	logparser "github.com/AntoineMeresse/flibot-urt/src/logs"
 	"github.com/AntoineMeresse/flibot-urt/src/models"
 	"github.com/AntoineMeresse/flibot-urt/src/vote"
-
-	log "github.com/sirupsen/logrus"
 )
 
 const Logfile = "/home/antoine/UrbanTerror43/q3ut4/games.log"
 const WorkerNumber = 3 // Param ?
 
-
-func initLogger() {
-	log.SetLevel(log.DebugLevel)
-	log.SetReportCaller(true)
-	log.SetFormatter(&log.TextFormatter{
-		FullTimestamp: true,
-		CallerPrettyfier: func(frame *runtime.Frame) (function string, file string) {                                                     
-			fileName := " " + path.Base(frame.File) + ":" + strconv.Itoa(frame.Line)  + " | "      
-			//return frame.Function, fileName                                        
-			return "", fmt.Sprintf("%25.25s", fileName)                                                      
-		},    
-		DisableLevelTruncation: true,
-		PadLevelText: true,
-	})
-	log.SetOutput(os.Stdout)
-}
-
 func main() {
-	initLogger()
+	configureLogger()
 
 	// Channels
 	myLogChannel := make(chan string)
