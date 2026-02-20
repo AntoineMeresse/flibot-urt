0727a56bceda5b6da28fe52f643a30f37f42ff69
Antoine Meresse <ant.meresse@gmail.com>
2026-02-20 18:34:45 +0100

    fix: quit command improve

--- src/commands/commands_list/quit.go
+++ src/commands/commands_list/quit.go
@@ -1,9 +1,15 @@
 package commandslist
 
 import (
+	"strings"
+
 	appcontext "github.com/AntoineMeresse/flibot-urt/src/context"
 )
 
 func Quit(cmd *appcontext.CommandsArgs) {
-	cmd.RconCommand("kick %s \"%s\"", cmd.PlayerId, "This player isn't good enough for this map !")
+	reason := "This player isn't good enough for this map !"
+	if len(cmd.Params) > 0 {
+		reason = strings.Join(cmd.Params, " ")
+	}
+	cmd.RconCommand("kick %s \"%s\"", cmd.PlayerId, reason)
 }
