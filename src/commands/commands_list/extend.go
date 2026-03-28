d0d69955659386b1c6551f9b32ab3eb3b85f8476
Antoine Meresse <ant.meresse@gmail.com>
2026-03-28 12:17:49 +0100

    feat: extend command vote

--- src/commands/commands_list/extend.go
+++ src/commands/commands_list/extend.go
@@ -1,28 +1,35 @@
 package commandslist
 
 import (
+	"fmt"
+
 	appcontext "github.com/AntoineMeresse/flibot-urt/src/context"
+	"github.com/AntoineMeresse/flibot-urt/src/models"
 	"github.com/AntoineMeresse/flibot-urt/src/utils"
 )
 
 func Extend(cmd *appcontext.CommandsArgs) {
-	extendTime := -1
+	extendTime := 60
 
-	if len(cmd.Params) == 0 {
-		extendTime = 60
-	} else if len(cmd.Params) > 0 {
+	if len(cmd.Params) > 0 {
 		t, err := utils.ExtractNumber(cmd.Params[0])
-
-		if err == nil {
-			if t > 0 && t < 1000 {
-				extendTime = t
-			}
+		if err != nil || t <= 0 || t >= 1000 {
+			cmd.RconUsage()
+			return
 		}
+		extendTime = t
 	}
 
-	if extendTime > 0 {
-		cmd.RconCommand("extend %d", extendTime)
+	player, err := cmd.Context.Players.GetPlayer(cmd.PlayerId)
+	if err != nil {
+		cmd.RconText("^1Could not identify your player.")
+		return
+	}
+
+	if player.Role <= 60 {
+		v := models.Vote{Params: []string{"extend", fmt.Sprintf("%d", extendTime)}, PlayerId: cmd.PlayerId}
+		cmd.Context.NewVote(v)
 	} else {
-		cmd.RconUsage()
+		cmd.RconCommand("extend %d", extendTime)
 	}
 }
