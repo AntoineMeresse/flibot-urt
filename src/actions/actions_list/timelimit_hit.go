1b0b488b53e3f1f2e463fadda1de600e680cdb74
Antoine Meresse <ant.meresse@gmail.com>
2026-03-28 10:20:07 +0100

    feat: remove custom messages from auto vote

--- src/actions/actions_list/timelimit_hit.go
+++ src/actions/actions_list/timelimit_hit.go
@@ -1,23 +1,12 @@
 package actionslist
 
 import (
-	"math/rand"
 	"time"
 
 	appcontext "github.com/AntoineMeresse/flibot-urt/src/context"
 	"github.com/AntoineMeresse/flibot-urt/src/models"
 	log "github.com/sirupsen/logrus"
 )
-
-var runningMessages = []string{
-	"I'm currently running, please wait !",
-	"I can feel it this time !",
-	"I was BORN to finish this run !",
-	"My fingers are sweating, extend pls",
-	"One more try I swear",
-	"I'm so close, don't kill my dreams",
-	"If you don't extend I will cry",
-}
 
 func TimelimitHit(actionParams []string, c *appcontext.AppContext) {
 	log.Debugf("Timelimit hit: %v", actionParams)
@@ -34,7 +23,6 @@ func announceRunningPlayers(c *appcontext.AppContext) {
 	time.Sleep(500 * time.Millisecond)
 	numbers := c.Runs.RunningPlayerNumbers()
 	for _, number := range numbers {
-		msg := runningMessages[rand.Intn(len(runningMessages))]
-		c.RconCommand("spoof %s say + %s [auto vote]", number, msg)
+		c.RconCommand("spoof %s say + [auto vote]", number)
 	}
 }
